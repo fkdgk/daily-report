d5bc9ce303d5059c16579d6d0b3947fd929877cf
fkdgk <fukuda@fukuda-gikou.co.jp>
2021-01-31 11:49:48 +0900

    gitflow-feature-stash: add_blanktemplate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Division;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = \Faker\Factory::create('ja_JP');
+
+        $sdivisions = [
+            "総務部", // 1
+            "人事部", // 2
+            "経理部", // 3
+            "営業部", // 4
+            "開発部", // 5
+            "事業部", // 6
+            "製造部", // 7
+        ];
+
+        foreach ($sdivisions as $sivision) {
+            Division::create(['name'=>$sivision]);
+        }
+
+        User::create([
+            'name' => $faker -> name,
+            'email' => 'demo@example.com',
+            'division_id' => 1,
+            'password' => bcrypt('pw1234'),
+        ]);
+
+        for ($i=0; $i < 10; $i++) { 
+            User::create([
+                'name' => $faker -> name,
+                'email' => $faker -> email,
+                'division_id' => $faker -> numberBetween(1,7),
+                'password' => bcrypt('pw1234'),
+            ]);
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,10 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', function() {
-    return view('home');
-})->name('home')->middleware('auth');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/users', [UserController::class, 'index'])->name('user.index');
