a47b74d33a4d0bcbf8b44d24bd67d76a471f8b6e
Fukuda Shuhei <fukuda@fukuda-gikou.co.jp>
2021-01-31 17:24:35 +0900

    upated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::orderBy('id','desc')->paginate(30);
+        $users = User::orderBy('id','desc')->get();
+        return view('home',[
+            'posts' => $posts,
+            'users' => $users,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,18 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <p class="mb-0">You are logged in!</p>
+                    <table class="table table-sm">
+                        <tbody>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{ $post -> user -> name }}</td>
+                                    <td>{{ $post -> work_date }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $posts->appends(request()->input())->links('pagination::bootstrap-4') }}
+                    {{ $posts->links('pagination::bootstrap-4') }}
                 </div>
             </div>
         </div>
