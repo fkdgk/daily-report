b03499ad438c351bd2c2a8638f9abef0d21e6031
fkdgk <fukuda@fukuda-gikou.co.jp>
2021-02-02 09:03:53 +0900

    ステータス有効のみ 表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,8 +25,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('id','desc')->paginate(15);
+
+        /* すべての投稿 */
+        // $posts = Post::orderBy('id','desc')->paginate(15);
+
+        /* ユーザが有効のみ */
+        $posts = Post::select('posts.*')
+        -> join('users','users.id','=','posts.user_id')
+        -> where('users.active', 1)
+        -> orderBy('posts.id','desc')
+        -> paginate(15);
+
         $users = User::orderBy('id','desc')->where('active',1)->get();
+        
         return view('home',[
             'posts' => $posts,
             'users' => $users,
