0964492e8c83578071b2f5f3e9c2eaf0de4743ae
Shuhei Fukuda <fukuda@fukuda-gikou.co.jp>
2021-02-06 20:19:55 +0900

    update validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -116,7 +116,7 @@ class PostController extends Controller
         $request->validate([
             'work_date' => 'required|date',
             'start_time' => 'date_format:H:i',
-            'finish_time' => 'date_format:H:i',
+            'finish_time' => 'date_format:H:i|after:start_time',
         ]);
     }
 
