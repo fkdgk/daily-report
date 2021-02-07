0f34a91802048441980cacd923ce528f74b91281
Shuhei Fukuda <fukuda@fukuda-gikou.co.jp>
2021-02-07 15:08:24 +0900

    valid update user

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,5 +1,29 @@
 <?php
+
 use Carbon\Carbon;
+use Intervention\Image\Facades\Image;
+
+/* プロフィール画像作成 */
+function makeUserImage($user){
+    /* Image Config */
+    $ext = 'jpg'; // jpg,png,gif,webp
+    $size = 150;
+    $quality = 80;
+    $save_path = 'img/';
+    $now = date('Ymd_His');
+    $user_id = $user -> id;
+    $file_name = $user_id . '_' . $now . '.' . $ext;
+
+    /* update image */
+    $img = request('img');
+    if ($img) {
+        Image::make($img)
+            -> fit($size)
+            -> encode($ext)
+            -> save(public_path($save_path) . $file_name, $quality);
+        $user -> img = $file_name;
+    }
+}
 
 function getUser($id){
     return App\Models\User::find($id);
