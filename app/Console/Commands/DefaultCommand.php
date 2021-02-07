5aa454f8bcb67f9a0dc24e52816a8844a3338720
Shuhei Fukuda <fukuda@fukuda-gikou.co.jp>
2021-02-07 13:50:38 +0900

    default command

--- app/Console/Commands/DefaultCommand.php
+++ app/Console/Commands/DefaultCommand.php
@@ -5,14 +5,14 @@ namespace App\Console\Commands;
 use File;
 use Illuminate\Console\Command;
 
-class DeleteUserImages extends Command
+class DefaultCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'delete:userimage';
+    protected $signature = 'command:default';
 
     /**
      * The console command description.
