35c977907751efe9e90f8ae522e7a5b4fbfc5849
fkdgk <fukuda@fukuda-gikou.co.jp>
2021-02-07 11:08:58 +0900

    upload toastr

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'img',
+        'role',
+        'active',
+        'division_id',
         'password',
     ];
 
